0bb3fc2ce2455aeec694cae7d97fd9220423c544
Dyfust <dyfust@gmail.com>
2025-06-14 13:19:33 +1000

    Initialised repo

---
+++ Assets/FirstPersonController.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public Camera playerCamera;
+
+    public float fov = 60f;
+    public bool cameraCanMove = true;
+    public float mouseSensitivity = 2f;
+    public float maxLookAngle = 50f;
+
+    // Internal Variables
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public bool playerCanMove = true;
+    public float walkSpeed = 5f;
+    public float maxVelocityChange = 10f;
+
+    // Internal Variables
+    private Vector3 _inputDirection;
+
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public float sprintSpeed = 7f;
+    public float sprintFOV = 80f;
+    public float sprintFOVStepTime = 10f;
+
+    public KeyCode jumpKey = KeyCode.Space;
+    public float jumpPower = 5f;
+
+    // Internal Variables
+    private bool isGrounded = false;
+
+    public Transform joint;
+    public float bobSpeed = 10f;
+    public Vector3 bobAmount = new Vector3(.15f, .05f, 0f);
+
+    // Internal Variables
+    private Vector3 jointOriginalPos;
+    private float timer = 0;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // Set internal variables
+        playerCamera.fieldOfView = fov;
+        jointOriginalPos = joint.localPosition;
+    }
+
+    float camRotation;
+
+    private void Update()
+    {
+        if (cameraCanMove)
+        {
+            yaw = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
+
+            // Clamp pitch between lookAngle
+            pitch = Mathf.Clamp(pitch, -maxLookAngle, maxLookAngle);
+
+            transform.localEulerAngles = new Vector3(0, yaw, 0);
+            playerCamera.transform.localEulerAngles = new Vector3(pitch, 0, 0);
+        }
+
+        // Calculate how fast we should be moving
+        _inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (_inputDirection.x != 0 || _inputDirection.z != 0 && isGrounded)
+        {
+            _currentState = State.Walking;
+            if (Input.GetKey(sprintKey))
+                _currentState = State.Sprinting;
+        }
+        else
+        {
+            _currentState = State.Still;
+        }
+
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+            Jump();
+
+        HeadBob();
+
+        if (_currentState == State.Sprinting)
+            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, sprintFOV, sprintFOVStepTime * Time.deltaTime);
+        else
+            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, fov, sprintFOVStepTime * Time.deltaTime);
+    }
+
+    private enum State { Sprinting, Airborne, Walking, Still }
+    private State _currentState;
+
+    void FixedUpdate()
+    {
+        CheckGround();
+
+        // All movement calculations shile sprint is active
+        if (_currentState == State.Sprinting)
+        {
+            _inputDirection = transform.TransformDirection(_inputDirection) * sprintSpeed;
+
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rb.linearVelocity;
+            Vector3 velocityChange = (_inputDirection - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        }
+        // All movement calculations while walking
+        else if (_currentState == State.Walking)
+        {
+            _inputDirection = transform.TransformDirection(_inputDirection) * walkSpeed;
+
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rb.linearVelocity;
+            Vector3 velocityChange = (_inputDirection - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        }
+    }
+
+    // Sets isGrounded based on a raycast sent straigth down from the player object
+    private void CheckGround()
+    {
+        Vector3 origin = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * .5f), transform.position.z);
+        Vector3 direction = transform.TransformDirection(Vector3.down);
+        float distance = .75f;
+
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, distance))
+        {
+            Debug.DrawRay(origin, direction * distance, Color.red);
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void Jump()
+    {
+        // Adds force to the player rigidbody to jump
+        if (isGrounded)
+        {
+            rb.AddForce(0f, jumpPower, 0f, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void HeadBob()
+    {
+        switch (_currentState)
+        {
+            case State.Walking:
+                timer += Time.deltaTime * bobSpeed;
+                joint.localPosition = new Vector3(jointOriginalPos.x + Mathf.Sin(timer) * bobAmount.x, jointOriginalPos.y + Mathf.Sin(timer) * bobAmount.y, jointOriginalPos.z + Mathf.Sin(timer) * bobAmount.z);
+                break;
+            case State.Sprinting:
+                timer += Time.deltaTime * (bobSpeed + sprintSpeed);
+                joint.localPosition = new Vector3(jointOriginalPos.x + Mathf.Sin(timer) * bobAmount.x, jointOriginalPos.y + Mathf.Sin(timer) * bobAmount.y, jointOriginalPos.z + Mathf.Sin(timer) * bobAmount.z);
+                break;
+            case State.Still:
+                timer = 0;
+                joint.localPosition = new Vector3(Mathf.Lerp(joint.localPosition.x, jointOriginalPos.x, Time.deltaTime * bobSpeed), Mathf.Lerp(joint.localPosition.y, jointOriginalPos.y, Time.deltaTime * bobSpeed), Mathf.Lerp(joint.localPosition.z, jointOriginalPos.z, Time.deltaTime * bobSpeed));
+                break;
+        }
+    }
+}
